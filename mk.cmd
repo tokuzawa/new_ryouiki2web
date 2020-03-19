0a1ddd114247892cc135f7d100499175596f8676
Satoshi Ohdachi <ohdachi@nifs.ac.jp>
2020-03-19 20:12:14 +0900

    css adjustment

--- mk.cmd
+++ mk.cmd
@@ -1,3 +1,4 @@
 pandoc --template=template.html --metadata pagetitle="index.html" testmain.md -o index.html
 pandoc --template=template.html --metadata pagetitle="links.html" links.md -o links.html
 pandoc --template=template.html --metadata pagetitle="mailinglist.html" mailinglist.md -o mailinglist.html
+pandoc --template=template.html --metadata pagetitle="daihyo_yakuin.html" daihyo_yakuin.md -o daihyo_yakuin.html
