c7134d2dad94fd71cd6cfe10f6053da71b406c41
Satoshi Ohdachi <ohdachi@nifs.ac.jp>
2020-03-19 18:42:41 +0900

    initial

---
+++ mk.cmd
@@ -0,0 +1,2 @@
+% pandoc --template=template.html --metadata pagetitle="index.html" testmain.md -o testmain.html
+pandoc --template=template.html --metadata pagetitle="links.html" links.md -o links.html
