2f1f845b52e3e757ba1f80bf4b23e4446d6b223c
Satoshi Ohdachi <ohdachi@nifs.ac.jp>
2020-03-26 16:55:29 +0900

    topical_session

--- mk.cmd
+++ mk.cmd
@@ -2,5 +2,6 @@ rem pandoc --template=template.html --metadata pagetitle="index.html" testmain.m
 rem pandoc --template=template.html --metadata pagetitle="links.html" links.md -o links.html
 rem pandoc --template=template.html --metadata pagetitle="mailinglist.html" mailinglist.md -o mailinglist.html
 rem pandoc --template=template.html --metadata pagetitle="daihyo_yakuin.html" daihyo_yakuin.md -o daihyo_yakuin.html
-pandoc --template=template.html --metadata pagetitle="keyword.html" keyword.md -o keyword.html
-pandoc --template=template.html --metadata pagetitle="keyword-e.html" keyword-e.md -o keyword-e.html
+rem pandoc --template=template.html --metadata pagetitle="keyword.html" keyword.md -o keyword.html
+rem pandoc --template=template.html --metadata pagetitle="keyword-e.html" keyword-e.md -o keyword-e.html
+pandoc --template=template.html --metadata pagetitle="topical_sesion.html" topical_session.md -o topical_session.html
