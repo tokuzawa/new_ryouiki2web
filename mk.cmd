a81d9c72d25e29701284edc32b630a1308567f3a
Satoshi Ohdachi <ohdachi@nifs.ac.jp>
2020-03-26 19:41:42 +0900

    before editing main.md

--- mk.cmd
+++ mk.cmd
@@ -1,7 +1,8 @@
-rem pandoc --template=template.html --metadata pagetitle="index.html" testmain.md -o index.html
-rem pandoc --template=template.html --metadata pagetitle="links.html" links.md -o links.html
-rem pandoc --template=template.html --metadata pagetitle="mailinglist.html" mailinglist.md -o mailinglist.html
-rem pandoc --template=template.html --metadata pagetitle="daihyo_yakuin.html" daihyo_yakuin.md -o daihyo_yakuin.html
-rem pandoc --template=template.html --metadata pagetitle="keyword.html" keyword.md -o keyword.html
-rem pandoc --template=template.html --metadata pagetitle="keyword-e.html" keyword-e.md -o keyword-e.html
+pandoc --template=template.html --metadata pagetitle="index.html" testmain.md -o index.html
+pandoc --template=template.html --metadata pagetitle="links.html" links.md -o links.html
+pandoc --template=template.html --metadata pagetitle="mailinglist.html" mailinglist.md -o mailinglist.html
+pandoc --template=template.html --metadata pagetitle="daihyo_yakuin.html" daihyo_yakuin.md -o daihyo_yakuin.html
+pandoc --template=template.html --metadata pagetitle="keyword.html" keyword.md -o keyword.html
+pandoc --template=template.html --metadata pagetitle="keyword-e.html" keyword-e.md -o keyword-e.html
 pandoc --template=template.html --metadata pagetitle="topical_sesion.html" topical_session.md -o topical_session.html
+pandoc --template=template.html --metadata pagetitle="meetinginfo.html" meetinginfo.md -o meetinginfo.html
