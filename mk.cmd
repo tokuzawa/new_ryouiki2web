6ae7cda06472d7cdeee7dc1b2a1db11245e4e528
Satoshi Ohdachi <ohdachi@nifs.ac.jp>
2020-05-09 23:41:35 +0900

    make it stabilized

--- mk.cmd
+++ mk.cmd
@@ -1,5 +1,4 @@
-rem pandoc --template=template.html --metadata pagetitle="index.html" testmain.md -o index.html
-pandoc --template=template.html --metadata pagetitle="links.html" links.md -o links.html
+rem pandoc --template=template.html --metadata pagetitle="links.html" links.md -o links.html
 rem pandoc --template=template.html --metadata pagetitle="mailinglist.html" mailinglist.md -o mailinglist.html
 rem pandoc --template=template.html --metadata pagetitle="daihyo_yakuin.html" daihyo_yakuin.md -o daihyo_yakuin.html
 rem pandoc --template=template.html --metadata pagetitle="keyword.html" keyword.md -o keyword.html
@@ -7,6 +6,7 @@ rem pandoc --template=template.html --metadata pagetitle="keyword-e.html" keywor
 rem pandoc --template=template.html --metadata pagetitle="topical_sesion.html" topical_session.md -o topical_session.html
 rem pandoc --template=template.html --metadata pagetitle="meetinginfo.html" meetinginfo.md -o meetinginfo.html
 rem pandoc --template=template.html --metadata pagetitle="index.html" main.md -o index.html
-pandoc --template=template.html --metadata pagetitle="symposium.html" symposium.md -o symposium.html
+rem pandoc --template=template.html --metadata pagetitle="symposium.html" symposium.md -o symposium.html
 rem pandoc --template=template.html --metadata pagetitle="r2meeting.html" r2meeting.md -o r2meeting.html
 rem pandoc --template=template.html --metadata pagetitle="boardmeeting.html" boardmeeting.md -o boardmeeting.html
+pandoc --template=template.html --metadata pagetitle="collaboration.html" collaboration.md -o collaboration.html
