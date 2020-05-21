f40430ec5165729855c0118c91bd11678d5703a8
Satoshi Ohdachi <ohdachi@nifs.ac.jp>
2020-05-21 20:44:45 +0900

    first version

--- addurl.rb
+++ addurl.rb
@@ -1,7 +1,7 @@
 #add url for r2 web
 Encoding.default_external = 'utf-8'
 fns = []
-outdir = 'output'
+outdir = 'docs/'
 urla = 'http://www.r2.div.jps.or.jp/'
 
 Dir.glob('*.md'){|fn|
@@ -21,28 +21,44 @@ fns.each{|fn|
 #print str, "\n"
 #print str.gsub(/\((.*?)\)/){|m| '(http'+$1+')'}
 
-str2 = ""
-
-File.open(fns[13]).each_line{|line|
-    if /\[.*\]\((.*?)\)/ =~ line
-        url = $1
-        if /http.*/ !~ url
-            line = line.gsub!(/\((.*?)\)/){|m| '(' + urla+$1+')'}
+fns.each{|fn|
+    # print fn, "  "
+    otitle = fn.gsub(/\.md/){|m| '.html'}
+    ofn = outdir+fn.gsub(/\.md/){|m| '.html'}
+    # print ofn, "\n"
+    str2 = ""
+    File.open(fn).each_line{|line|
+        if /\[.*\]\((.*?)\)/ =~ line
+            url = $1
+            if /http.*/ !~ url
+                line = line.gsub!(/\((.*?)\)/){|m| '(' + urla+$1+')'}
+            end
         end
-    end
-    if /src *= *\"(.*?)\"/ =~ line    
-        url = $1
-        if /http.*/ !~ url
-            line = line.gsub!(/src *= *\"(.*?)\"/){|m| 'src="' + urla+$1+'"'}
-        end
-    end 
-    if /href *= *\"(.*?)\"/ =~ line    
-        url = $1
-        if /http.*/ !~ url
-            line = line.gsub!(/href *= *\"(.*?)\"/){|m| 'href="' + url+$1+'"'}
+        if /src *= *\"(.*?)\"/ =~ line    
+            url = $1
+            if /http.*/ !~ url
+                line = line.gsub!(/src *= *\"(.*?)\"/){|m| 'src="' + urla+$1+'"'}
+            end
         end 
+        if /href *= *\"(.*?)\"/ =~ line    
+            url = $1
+            if /http.*/ !~ url
+                line = line.gsub!(/href *= *\"(.*?)\"/){|m| 'href="' + url+$1+'"'}
+            end 
+        end 
+        str2 += line
+    }
+
+    f=File.open('temp.md', 'w')
+    f.puts(str2)
+    f.close()
+
+    if otitle == "main.html"
+        otitle = "index.html"
+        ofn = outdir + otitle
     end 
-    str2 += line
+    cmd = "pandoc --template=template.html --metadata pagetitle=\"#{otitle}\" temp.md -o #{ofn}"
+    print cmd, "\n"
+    system(cmd)
 }
 
-File.open('temp.md', 'w').puts(str2)
